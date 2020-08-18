568911453d7a3b9ffc624853e263be0f6f68af89
Daksh Pokar <dakshpokar@gmail.com>
2020-08-18 13:11:00 +0530

    Dynamic Data

---
+++ data.js
@@ -0,0 +1,33 @@
+module.exports = function(){
+    return {
+        products:[
+            {id: 1, name: "Kayak", category: "Watersports", 
+                description: "A boat for one person", price: 275, url: "https://picsum.photos/600/400?a"},
+
+            {id: 2, name: "Lifejacket", category: "Watersports", 
+                description: "A jacket for saving one person", price: 48.95, url: "https://picsum.photos/600/400?a"},
+
+            {id: 3, name: "Soccer Ball", category: "Soccer", 
+                description: "FIA approved size and weights", price: 19.50, url: "https://picsum.photos/600/400?a"},
+
+            {id: 4, name: "Corner Flags", category: "Soccer", 
+                description: "Give your field a professional touch", price: 300, url: "https://picsum.photos/600/400?a"},
+
+            {id: 5, name: "Stadium", category: "Watersports", 
+                description: "Flat-packed 34000-seat stadium", price: 81000, url: "https://picsum.photos/600/400?a"},
+
+            {id: 6, name: "Thinking Cap", category: "Chess", 
+                description: "Improve brain effectiveness by 89%", price: 16, url: "https://picsum.photos/600/400?a"},
+
+            {id: 7, name: "Unsteady Chair", category: "Chess", 
+                description: "Secretly give disadvantage to your opponent", price: 29.96, url: "https://picsum.photos/600/400?a"},
+
+            {id: 8, name: "Human Chess Board", category: "Chess", 
+                description: "A fun game for the family", price: 75, url: "https://picsum.photos/600/400?a"},
+
+            {id: 9, name: "Bling Bling King", category: "Chess", 
+                description: "Gold-plated, diamond studded king", price: 1300, url: "https://picsum.photos/600/400?a"},
+                
+        ]
+    }
+}
